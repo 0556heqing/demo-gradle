c535ad135ca2d4737b304daf4825cc49400a86be
heqing <heqing@yiyaowang.com>
2020-05-09 15:15:22 +0800

    测试

---
+++ gradle_service/src/test/java/com/heqing/demo/service/TestHello.java
@@ -0,0 +1,11 @@
+package com.heqing.demo.service;
+
+import org.junit.Test;
+
+public class TestHello {
+
+    @Test
+    public void test() {
+        System.out.println("-->.hello world!");
+    }
+}
